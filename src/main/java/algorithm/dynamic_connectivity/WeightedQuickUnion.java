d2182e8ffc9360cbdd25486880b774875833be15
dincau33 <dincau33@gmail.com>
2018-09-02 10:11:05 -0400

    Dynamic_Connectivity problem: implement WeightedQuickUnion problem

--- src/main/java/algorithm/dynamic_connectivity/WeightedQuickUnion.java
+++ src/main/java/algorithm/dynamic_connectivity/WeightedQuickUnion.java
@@ -6,19 +6,23 @@ public class WeightedQuickUnion {
 	// 2 nodes are part of the same component if they have the same root
 	// id[i] represents the parent of node i
 	// id[i] = i if the node is not connected to any other node.
-	// Space complexity: count (int) + id (int[]) + height (int[])
+	// Space complexity: count (int) + id (int[]) + size (int[])
 	// = 4bytes + (N * 4bytes + 24bytes) + (N * 1byte + 24bytes)
 	// 24 bytes = (Array length 8bytes + Reference to Object class 8bytes + GC flags 8bytes)
 	private int[] id;
 	private int count;
-	private byte[] height;
+	private byte[] size;
 
 	// Time complexity: O(N)
 	public WeightedQuickUnion(int N) {
 		if (N < 0) throw new IllegalArgumentException();
 		count = N;
 		id = new int[N];
-		for (int i = 0; i < N; i++) id[i] = i;
+		size = new byte[N];
+		for (int i = 0; i < N; i++) {
+			id[i] = i;
+			size[i] = 1;
+		}
 	}
 
 	// Time complexity: O(N)
@@ -32,7 +36,14 @@ public class WeightedQuickUnion {
 	public void union(int p, int q) {
 		int rootp = find(p);
 		int rootq = find(q);
-		id[rootq] = rootp;
+		if (size[rootp] > size[rootq]) {
+			id[rootq] = rootp;
+			size[rootp] += size[rootq];
+		} else {
+			id[rootp] = rootq;
+			size[rootq] += size[rootp];
+		}
+
 	}
 
 	// Time complexity: O(N)
